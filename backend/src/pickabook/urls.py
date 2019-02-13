a2568ac92d9b0a33881b6784c8d24ccf69b2af27
killerbee4992 <lramados@purdue.edu>
2019-02-12 22:53:34 -0500

    One to One Profile Backend Done.

--- backend/src/pickabook/urls.py
+++ backend/src/pickabook/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('books.api.urls'))
+    path('library/', include('books.api.urls')),
+    path('profile/', include('users.api.urls')),
 ]
 
