7a397d41167e6d55325d232c2da58df80d98062c
killerbee4992 <lramados@purdue.edu>
2019-02-14 00:39:41 -0500

    Front end Component form for Profile.

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -4,6 +4,7 @@ import CustomLayout from './containers/Layout';
 import Login from './components/Form';
 import CenteredLayout from './containers/LoginLayout';
 import Signup from './containers/Signup';
+import ProfileRegister from './containers/ProfileRegister';
 
 const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
     <Route {...rest} render={props => (
@@ -21,6 +22,7 @@ const BaseRouter = (props) => (
         <AppRoute exact path='/' layout={(props) => <CustomLayout {...props} />} component={Login}/>
         <AppRoute exact path='/login/' layout={(props) => <CenteredLayout {...props} />} component={Login}/>
         <AppRoute exact path='/signup/' layout={(props) => <CenteredLayout {...props} />} component={Signup}/>
+        <AppRoute exact path='/newprof/' layout={(props) => <CustomLayout {...props} />} component={ProfileRegister}/>
       </Switch>
     </BrowserRouter>
     </div>
