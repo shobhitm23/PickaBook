ecb5f1bc5c4e410d70be78ef3e27a1bde80a030f
Piyush Juneja <pjuneja@purdue.edu>
2019-02-19 21:33:48 -0500

    added book reviews to the book detail view

--- frontend/gui/src/containers/BookDetailView.js
+++ frontend/gui/src/containers/BookDetailView.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { Card, Row, Col, List, message, Avatar, Spin } from "antd";
+import reqwest from 'reqwest';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const gridStyle = {
     textAlign: 'center',
@@ -18,13 +20,18 @@ class BookDetail extends React.Component {
             MY: "Mystery",
             BI: "Biography",
             FI: "Fiction",
-            SF: "Science Fiction"
+            SF: "Science Fiction",
+            reviews: [],
+            loading: false,
+            hasMore: true
         };
     }  
 
     componentDidMount = () => {
+        
         const bookID = this.props.match.params.bookID;
         this._isMounted=true;
+        
         axios.get(`http://127.0.0.1:8000/library/booklist/${bookID}`).then(res => {
           this.setState({
             // book: res.data
@@ -39,7 +46,36 @@ class BookDetail extends React.Component {
           });
         })
         .catch(error => console.log(error));
-    }  
+
+        axios.get(`http://127.0.0.1:8000/bookreview/${bookID}`).then(res => {
+            this.setState({
+                reviews: res.data,
+                max_length: res.data.length,
+                current_length: Math.min(3, res.data.length)
+            })
+            console.log(this.state)
+        })
+        .catch(error => console.log(error));
+    }
+
+    handleInfiniteOnLoad = () => {
+        let data = this.state.data;
+        this.setState({
+          loading: true,
+          current_length: Math.min(this.state.current_length+3, this.state.max_length)
+        });
+        console.log(this.state)
+
+        if (this.state.current_length >= this.state.max_length) {
+          message.warning('Infinite List loaded all');
+          this.setState({
+            hasMore: false,
+            loading: false,
+          });
+          return;
+        }
+    }
+    
 
     componentWillUnmount(){
         this._isMounted = false;
@@ -102,6 +138,39 @@ class BookDetail extends React.Component {
                                 <p>
                                     {this.state.synopsis}
                                 </p>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                    <Row gutter={20} type="flex" justify="center">
+                        <Col span={22}>
+                            <Card style={gridStyle} title="Reviews">
+                                
+                                <InfiniteScroll
+                                    initialLoad={false}
+                                    pageStart={0}
+                                    loadMore={this.handleInfiniteOnLoad}
+                                    hasMore={!this.state.loading && this.state.hasMore}
+                                    useWindow={false}
+                                >
+
+                                <List
+                                    dataSource={this.state.reviews.slice(0, this.state.current_length)}
+                                    renderItem={item => (
+                                    <List.Item key={item.id}>
+                                        <List.Item.Meta
+                                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                            title={<a href="https://ant.design">{item.title}</a>}
+                                            description={item.content}
+                                        />
+                                        <div>{item.creation_date}</div>
+                                        </List.Item>
+                                    )}
+                                >
+
+                                </List>
+                                </InfiniteScroll>
+
                             </Card>
                         </Col>
                     </Row>
