5988d5f07c0a7f53f3afcdee098c9b38b2f7fac4
killerbee4992 <lramados@purdue.edu>
2019-02-21 13:42:31 -0500

    Front end tests.

--- frontend/gui/src/App.test.js
+++ frontend/gui/src/App.test.js
@@ -1,3 +1,4 @@
+const axios  = require('axios');
 /*
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -14,6 +15,62 @@ it('renders without crashing', () => {
 test('Fake Test', () => {
     expect(true).toBeTruthy();
 })
+
+test('Fetch Book by name', () => {
+  expect.assertions(1);
+  return axios.get('http://127.0.0.1:8000/library/view/Tailspin')
+  .then(res => {
+      expect(res.data.title).toEqual('Tailspin');
+//      console.log(res.data);
+  })
+  .catch(err => console.log(err));
+});
+
+test('Fetch Author by name', () => {
+  expect.assertions(1);
+  return axios.get('http://127.0.0.1:8000/authors/view/Jordan')
+  .then(res => {
+      expect(res.data.name).toEqual('Jordan');
+//      console.log(res.data);
+  })
+  .catch(err => console.log(err));
+});
+
+test('Books By Author', () => {
+  expect.assertions(1);
+
+  return axios.get(`http://127.0.0.1:8000/library/authorbooks/Jordan`)
+      .then(res2 => {
+//        console.log(res2.data[0]);
+        expect('Test book 1').toBe(res2.data[0].title);
+      })
+      .catch(error => console.log(error));
+});
+
+test('Profile From User', () => {
+  expect.assertions(1);
+  return axios.get(`http://127.0.0.1:8000/profile/8`).then(res => {
+            expect(res.data.first_name).toBe('CS491');
+        });
+});
+
+test('Reviews of book', () => {
+  expect.assertions(2);
+  return axios.get(`http://127.0.0.1:8000/bookreview/1`).then(res => {
+          expect(res.data.length).toBeGreaterThan(0);
+          expect(res.data[0].title).toBe("Good stuff");
+  })
+})
+
+test('Questions of Book', () => {
+  expect.assertions(1);
+  return axios.get(`http://127.0.0.1:8000/qanswer/question/1`).then(res => {
+          expect(res.data.length).toBeGreaterThan(0);
+  });
+});
+
+
+
 /*
 describe('Indicator', () => {
   describe('when loading is false', () => {
